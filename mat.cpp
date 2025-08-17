a1f6cb60ff121322ee87094d0f9b50b0fbd58169
KaguyaH9 <kaguyah9@outlook.com>
2025-08-17 16:22:50 +0800

    template <Mod>

--- mat.cpp
+++ mat.cpp
@@ -5,39 +5,39 @@
 namespace kh {
   using namespace std;
   inline namespace src {
-    int matmod;
-    class modmat {
-      int r, c;
-      vector<vector<int>> m;
-      public:
-      modmat(int const r, int const c)
-        : r(r), c(c), m(r, vector<int>(c)) {}
-      modmat(int const r, int const c, int const x)
-        : modmat(r, c) {
-          assert(r == c);
-          for (int i(0); i != r; ++i) m[i][i] = x;
+    template <int M>
+      class mat {
+        int r, c;
+        vector<vector<int>> m;
+        public:
+        mat(int const r, int const c)
+          : r(r), c(c), m(r, vector<int>(c)) {}
+        mat(int const r, int const c, int const x)
+          : mat(r, c) {
+            assert(r == c);
+            for (int i(0); i != r; ++i) m[i][i] = x;
+          }
+        int& operator()(int const x, int const y) { return m[x][y]; }
+        int operator()(int const x, int const y) const { return m[x][y]; }
+        friend mat operator+(mat const& a, mat const& b) {
+          assert(a.r == b.r);
+          assert(a.c == b.c);
+          mat c(a.r, a.c);
+          for (int i(0); i != a.r; ++i)
+            for (int j(0); j != a.c; ++j)
+              c(i, j) = (a(i, j) + b(i, j)) % M;
+          return c;
         }
-      int& operator()(int const x, int const y) { return m[x][y]; }
-      int operator()(int const x, int const y) const { return m[x][y]; }
-      friend modmat operator+(modmat const& a, modmat const& b) {
-        assert(a.r == b.r);
-        assert(a.c == b.c);
-        modmat c(a.r, a.c);
-        for (int i(0); i != a.r; ++i)
-          for (int j(0); j != a.c; ++j)
-            c(i, j) = (a(i, j) + b(i, j)) % matmod;
-        return c;
-      }
-      friend modmat operator*(modmat const& a, modmat const& b) {
-        assert(a.c == b.r);
-        modmat c(a.r, b.c);
-        for (int i(0); i != a.r; ++i)
-          for (int j(0); j != a.c; ++j)
-            for (int k(0); k != b.c; ++k)
-              c(i, k) = (c(i, k) + 1l * a(i, j) * b(j, k)) % matmod;
-        return c;
-      }
-    };
+        friend mat operator*(mat const& a, mat const& b) {
+          assert(a.c == b.r);
+          mat c(a.r, b.c);
+          for (int i(0); i != a.r; ++i)
+            for (int j(0); j != a.c; ++j)
+              for (int k(0); k != b.c; ++k)
+                c(i, k) = (c(i, k) + 1l * a(i, j) * b(j, k)) % M;
+          return c;
+        }
+      };
   }
 }
 
@@ -45,18 +45,18 @@ int main() {
   using namespace std;
   using namespace kh;
   constexpr int P(1'000'000'007);
-  matmod = P;
+  typedef mat<P> mat;
   int n, p, m;
   cin >> n >> p >> m;
-  modmat a(n, p);
-  modmat b(p, m);
+  mat a(n, p);
+  mat b(p, m);
   for (int i(0); i != n; ++i)
     for (int j(0); j != p; ++j)
       cin >> a(i, j);
   for (int i(0); i != p; ++i)
     for (int j(0); j != m; ++j)
       cin >> b(i, j);
-  modmat const c(a * b);
+  mat const c(a * b);
   for (int i(0); i != n; ++i)
     for (int j(0); j != m; ++j)
       cout << (c(i, j) + P) % P << " \n"[j == m - 1];
