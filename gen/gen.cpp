f6c28854217348cc863623365401898ea2a74981
KaguyaH9 <kaguyah9@outlook.com>
2025-10-18 23:08:19 +0800

    Sat 18 Oct 2025 11:08:19 PM CST

--- gen/gen.cpp
+++ gen/gen.cpp
@@ -3,24 +3,38 @@
 int main() {
   using namespace std;
   namespace fs = filesystem;
-  fs::path path(".");
+  freopen("output/sources.tex", "w", stdout);
+  fs::path path("main");
   auto const cat = [] (auto const& entry) {
     ifstream fin(entry.path());
     string line;
     while (getline(fin, line))
-      cout << line << endl;
+      puts(line.c_str());
   };
+  auto const cvt = [] (string filename) {
+    for (size_t i(filename.size() - 1); ~i; --i)
+      if (filename[i] == '_')
+        filename.insert(next(filename.begin(), i), '\\');
+    return filename;
+  };
+  puts("\\documentclass[a4paper]{article}");
+  putchar('\n');
+  puts("\\usepackage[a4paper, left=1in, right=1in, top=1in, bottom=1in]{geometry}");
+  putchar('\n');
+  puts("\\begin{document}");
+  putchar('\n');
+  puts("\\small");
   for (auto const& entry: fs::directory_iterator(path)) {
-    if (entry.path().filename().string().front() == '.') continue;
-    if (entry.path().filename().string() == "gen") continue;
-    if (entry.path().filename().string() == "output") continue;
-    cout << "##" << ' ' << entry.path().filename().string() << endl;
-    cout << endl;
-    cout << "```" << endl;
+    putchar('\n');
+    printf("\\section{%s}\n", cvt(entry.path().filename().string()).c_str());
+    putchar('\n');
+    puts("\\begin{verbatim}");
     cat(entry);
-    cout << "```" << endl;
-    cout << endl;
-    cout << "<div style=\"page-break-after: always;\"></div>" << endl;
-    cout << endl;
+    puts("\\end{verbatim}");
+    putchar('\n');
+    puts("\\clearpage");
+    fflush(stdout);
   }
+  putchar('\n');
+  puts("\\end{document}");
 }
--- makefile
+++ makefile
@@ -1,2 +1,6 @@
+output/sources.pdf: output/sources.tex
+	xelatex --output-directory=output output/sources.tex
+output/sources.tex: gen/gen
+	gen/gen
 gen/gen: gen/gen.cpp
 	bash -ic 'dg1z gen/gen.cpp -o gen/gen'
